8476a0f8af4d26830debc3751f63296b8367d3ec
Olalekan Rasaq <orasaq377@stu.ui.edu.ng>
2026-03-16 18:59:11 +0100

    Updated data lake and IAM provisioning

---
+++ eu-central-1/staging/iam_updated.tf
@@ -0,0 +1,49 @@
+#Terraform does not manage the `forge-data-engineers` IAM group as far as I'm aware
+
+data "aws_iam_group" "forge_team" {
+  group_name = "forge-data-engineers"
+}
+
+
+resource "aws_iam_policy" "forge_team_staging_access" {
+  name        = "forge_staging_s3-access"
+  description = "Allow read + write to staging bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action = ["s3:ListBucket"]
+        Resource = [module.forge_data_lake.arn]
+        # we leverage the output from the module for the bucket arn
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject"
+        ]
+        Resource = [
+          module.forge_data_lake.arn,
+          "${module.forge_data_lake.arn}/*" 
+        ]
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter"
+        ]
+        Resource = "arn:aws:ssm:eu-central-1:049417293525:parameter/production/google-service-account/credentials"
+      }
+      # The SSM parameter seems to belong to prod
+    ]
+  })
+}
+
+resource "aws_iam_group_policy_attachment" "forge_team_staging_policy" {
+  group      = data.aws_iam_group.forge_team.group_name
+  policy_arn = aws_iam_policy.forge_team_staging_access.arn
+}
