2a96d98840bb3148bf895fe2d527be35f57bd5b1
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2026-03-17 19:31:49 +0100

    add ECR lifecycle policy to expire unused images in forge airflow repo

--- eu-central-1/production/ecr.tf
+++ eu-central-1/production/ecr.tf
@@ -8,3 +8,26 @@ resource "aws_ecr_repository" "forge_airflow" {
 
   tags = merge(local.common_tags, { Name = "forge-airflow" })
 }
+
+resource "aws_ecr_lifecycle_policy" "expire_unused_images" {
+  repository = aws_ecr_repository.forge_airflow.name
+
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 3 images only",
+        "selection": {
+          "tagStatus": "any",
+          "countType": "imageCountMoreThan",
+          "countNumber": 3
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+  EOF
+}
