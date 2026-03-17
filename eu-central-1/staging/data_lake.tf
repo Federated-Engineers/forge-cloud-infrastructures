490ff4638e3c8afcc4975ab7bdd5fa493a65998d
desayoem <labaekaadesayo@gmail.com>
2026-03-17 23:50:45 +0000

    Provision data lake and generic IAM access for forge team

---
+++ eu-central-1/staging/data_lake.tf
@@ -0,0 +1,9 @@
+module "forge_data_lake" {
+    source = "../modules/s3-bucket"
+
+    environment = var.environment
+    team = var.team
+    bucket-use-case = "datalake"
+    service = "airflow"
+    versioning = var.versioning
+}
---
+++ eu-central-1/staging/iam.tf
@@ -0,0 +1,40 @@
+data "aws_iam_group" "forge_team" {
+  group_name = "forge-data-engineers"
+}
+
+
+resource "aws_iam_policy" "forge-team-generic-access" {
+  name        = "forge_team_generic_access"
+  description = "Forge team access policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action = ["s3:ListBucket"]
+        Resource = [module.forge_data_lake.arn]
+      },
+
+      {
+        Effect = "Allow"
+        Action = ["s3:*Object"]
+        Resource = "${module.forge_data_lake.arn}/*"
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter"
+        ]
+        Resource = "arn:aws:ssm:eu-central-1:049417293525:parameter/production/google-service-account/credentials"
+      }
+
+    ]
+  })
+}
+
+resource "aws_iam_group_policy_attachment" "forge_team_staging_policy" {
+  group      = data.aws_iam_group.forge_team.group_name
+  policy_arn = aws_iam_policy.forge-team-generic-access.arn
+}
