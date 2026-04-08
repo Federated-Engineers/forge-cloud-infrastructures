09a2141ecdc9e137ce4d5e7eacbd274429db821b
desayoem <labaekaadesayo@gmail.com>
2026-04-08 22:40:31 +0100

    Create forge user and credentials for staging operations
    - Create forge_generic_user for general access to AWS resources during development
    - Attach user to existing forge-team-generic-access
    - Create and output access and secret key for forge_generic_user

--- eu-central-1/staging/iam.tf
+++ eu-central-1/staging/iam.tf
@@ -38,3 +38,31 @@ resource "aws_iam_group_policy_attachment" "forge_team_staging_policy" {
   group      = data.aws_iam_group.forge_team.group_name
   policy_arn = aws_iam_policy.forge-team-generic-access.arn
 }
+
+resource "aws_iam_user" "forge_generic_user" {
+  name = "forge-user"
+
+  tags = {
+    Team = "forge"
+    Environment = "staging"
+  }
+}
+
+resource "aws_iam_access_key" "forge_generic_user" {
+  user = aws_iam_user.forge_generic_user.name
+}
+
+output "forge_generic_access_key_id" {
+  value     = aws_iam_access_key.forge_generic_user.id
+  sensitive = true
+}
+
+output "forge_generic_secret_access_key" {
+  value     = aws_iam_access_key.forge_generic_user.secret
+  sensitive = true
+}
+
+resource "aws_iam_user_policy_attachment" "forge_user_staging_policy" {
+  user       = aws_iam_user.forge_generic_user.name
+  policy_arn = aws_iam_policy.forge-team-generic-access.arn
+}
