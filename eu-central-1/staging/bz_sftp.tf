ba8767fe52d6f5bc79759b8a4de3c23c1aff8852
ojo <ojokayode13@gmail.com>
2026-04-13 13:23:06 +0100

    Blocked s3 public access to secure the bucket for the sftp transfer

--- eu-central-1/staging/bz_sftp.tf
+++ eu-central-1/staging/bz_sftp.tf
@@ -37,3 +37,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bz_sftp_bucket_en
   }
 }
 
+# -------------------------------------------------------------
+# S3 PUBLIC ACCESS BLOCK
+# -------------------------------------------------------------
+resource "aws_s3_bucket_public_access_block" "bz_sftp_bucket_public_access" {
+  bucket = module.bz_sftp_bucket.bucket_name
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
