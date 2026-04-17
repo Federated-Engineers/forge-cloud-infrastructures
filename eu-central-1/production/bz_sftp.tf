9d481515a6e4b0b2b0f01b08acd6428767811e9e
ojo <ojokayode13@gmail.com>
2026-04-17 16:52:48 +0100

    Chnaged the bucket configuration use-case and team

--- eu-central-1/production/bz_sftp.tf
+++ eu-central-1/production/bz_sftp.tf
@@ -4,8 +4,8 @@ module "bz_sftp_bucket" {
   source = "../modules/s3-bucket"
 
   environment     = var.environment
-  team            = var.team
-  bucket-use-case = "sftp"
+  team            = "forge"
+  bucket-use-case = "bz-sftp"
   service         = "transfer-family"
   versioning      = "Enabled"
 }
