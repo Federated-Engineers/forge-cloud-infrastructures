79e6833c21c4fcf40d067b6d57a030402c296aea
poshthagoat <olupelumioshundiya@gmail.com>
2026-04-18 11:36:42 +0100

    add bbss_forecasts infrastructure

---
+++ eu-central-1/production/bbss_forecasts.tf
@@ -0,0 +1,12 @@
+# Balearic Blue Superyacht Services (BBSS) Infra
+
+module "bbss_weather_bucket" {
+  source = "../modules/s3-bucket"
+
+  environment     = var.environment
+  team            = "forge"
+  bucket-use-case = "bbss-forecasts"
+  service         = "airflow"
+  versioning      = "Enabled"
+
+}
