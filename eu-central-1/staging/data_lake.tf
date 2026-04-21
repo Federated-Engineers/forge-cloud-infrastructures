59902fdb3f9266ddbaf7026870cc02232b58f8d0
posh <olupelumioshundiya@gmail.com>
2026-04-22 00:47:58 +0100

    migrate BBSS weather bucket configuration from staging to production environment

--- eu-central-1/staging/data_lake.tf
+++ eu-central-1/staging/data_lake.tf
@@ -8,14 +8,4 @@ module "forge_data_lake" {
   versioning      = var.versioning
 }
 
-# Balearic Blue Superyacht Services (BBSS) Infra
-module "bbss_weather_bucket" {
-  source = "../modules/s3-bucket"
 
-  environment     = var.environment
-  team            = var.team
-  bucket-use-case = "bbss-forecasts"
-  service         = "airflow"
-  versioning      = var.versioning
-
-}
