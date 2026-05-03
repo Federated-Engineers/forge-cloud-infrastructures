d5e11880ff327b608a7c96333e1e5302417b43ed
ojo <ojokayode13@gmail.com>
2026-05-03 20:24:53 +0100

    Created a password for the cluster and put it in a ssm parameter

--- eu-central-1/production/lh_redshift.tf
+++ eu-central-1/production/lh_redshift.tf
@@ -1,10 +1,23 @@
+resource "random_password" "password" {
+  length  = 16
+  special = true
+}
+
+resource "aws_ssm_parameter" "redshift_master_password" {
+  name        = "/${var.environment}/${var.team}/redshift/lief-holdings/master-password"
+  description = "Master password for the Lief Holdings Redshift cluster"
+  type        = "SecureString"
+  value       = random_password.lief_holdings_password.result
+}
+
 resource "aws_redshift_cluster" "lief_holdings" {
-  cluster_identifier = "lief-holdings-predictive-pricing"
-  database_name      = "pricing_db"
-  master_username    = "exampleuser"
-  master_password    = "Mustbe8characters"
-  node_type          = "ra3.large"
-  cluster_type       = "single-node"
+  cluster_identifier  = "lief-holdings-predictive-pricing"
+  database_name       = "pricing_db"
+  master_username     = "exampleuser"
+  master_password     = "Mustbe8characters"
+  node_type           = "ra3.large"
+  cluster_type        = "single-node"
+  publicly_accessible = true
 
   tags = merge(local.common_tags, { client = "lief-holdings" })
 }
