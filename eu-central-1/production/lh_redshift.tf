e71c4ddb87eadd946881b903c42cc1895264b0f1
ojo <ojokayode13@gmail.com>
2026-05-04 09:34:55 +0100

    Added the redshift parameter group where the wlm configuration resides

--- eu-central-1/production/lh_redshift.tf
+++ eu-central-1/production/lh_redshift.tf
@@ -30,6 +30,84 @@ resource "aws_iam_role" "lief_holdings_redshift" {
   tags = merge(local.common_tags, { client = "lief-holdings" })
 }
 
+resource "aws_redshift_parameter_group" "lief_holdings_group" {
+  name   = "parameter-group-lief-holdings-terraform"
+  family = "redshift-1.0"
+
+  parameter {
+    name  = "require_ssl"
+    value = "true"
+  }
+
+  parameter {
+    name  = "enable_user_activity_logging"
+    value = "true"
+  }
+
+  parameter {
+    name = "wlm_json_configuration"
+    value = jsonencode([{
+      short_query_queue = true
+      },
+      {
+        name                  = "ETL Queue"
+        memory_percent_to_use = 40
+        query_concurrency     = 3
+        user_group            = ["etl_users"]
+        rules = [
+          {
+            rule_name = "abort_large_scan"
+            predicate = [
+              {
+                metric_name = "query_blocks_read"
+                operator    = ">"
+                value       = 2097152
+              }
+            ]
+            action = "abort"
+          }
+        ]
+      },
+      {
+        name                  = "Analyst/Ad-Hoc Queue"
+        memory_percent_to_use = 60
+        query_concurrency     = 5
+        concurrency_scaling   = "auto"
+        rules = [
+          {
+            rule_name = "abort_large_scan"
+            predicate = [
+              {
+                metric_name = "query_blocks_read"
+                operator    = ">"
+                value       = 2097152
+              }
+            ]
+            action = "abort"
+          },
+          {
+            rule_name = "downgrade_heavy_cpu"
+            predicate = [
+              {
+                metric_name = "query_cpu_usage_percent"
+                operator    = ">"
+                value       = 80
+              },
+              {
+                metric_name = "query_cpu_time"
+                operator    = ">"
+                value       = 1200
+              }
+            ]
+            action = "change_query_priority"
+            value  = "lowest"
+          }
+        ]
+      }
+    ])
+  }
+}
+
 resource "aws_redshift_cluster" "lief_holdings" {
   cluster_identifier  = "lief-holdings-predictive-pricing"
   database_name       = "pricing_db"
