8ef76dcf5d5fce59fc4fa0b56799d4dc96ccbfdf
Onidajo Fikayo <onidajo99@gmail.com>
2026-05-24 12:17:32 +0100

    Enable force_destroy for S3 bucket
    Added force_destroy option to S3 bucket resource.

--- eu-central-1/modules/s3-bucket/main.tf
+++ eu-central-1/modules/s3-bucket/main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "federated-engineers-bucket" {
-  bucket = "federated-engineers-${var.environment}-${var.team}-${var.bucket-use-case}"
+  bucket        = "federated-engineers-${var.environment}-${var.team}-${var.bucket-use-case}"
+  force_destroy = true
 
   tags = merge(local.common_tags, {
     Name    = "federated-engineers-${var.environment}-${var.team}-${var.bucket-use-case}",
