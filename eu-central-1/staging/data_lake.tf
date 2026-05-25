ba5fb7c07c06737b56a38cf2bcb8e65e1379a350
unknown <ojokayode13@gmail.com>
2026-05-25 13:23:58 +0100

    Changed the environment to production

--- eu-central-1/staging/data_lake.tf
+++ eu-central-1/staging/data_lake.tf
@@ -7,13 +7,3 @@ module "forge_data_lake" {
   service         = "airflow"
   versioning      = var.versioning
 }
-
-module "lonestar_tfstate" {
-  source = "../modules/s3-bucket"
-
-  environment     = var.environment
-  team            = "forge"
-  bucket-use-case = "lonestar-tf-state"
-  service         = "Terraform"
-  versioning      = "Enabled"
-}
