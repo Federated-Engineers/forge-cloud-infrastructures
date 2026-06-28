2c56b6423e384aea2898418f5a817909fd671fdb
RofiatAbdulkareem <oyizakareem9@gmail.com>
2026-06-28 10:59:14 +0100

    destroying resources

--- eu-central-1/production/data_lake.tf
+++ eu-central-1/production/data_lake.tf
@@ -1,51 +1,51 @@
-# Balearic Blue Superyacht Services (BBSS) Infra
+# # Balearic Blue Superyacht Services (BBSS) Infra
 
-module "bbss_weather_bucket" {
-  source = "../modules/s3-bucket"
+# module "bbss_weather_bucket" {
+#   source = "../modules/s3-bucket"
 
-  environment     = var.environment
-  team            = "forge"
-  bucket-use-case = "bbss-forecasts"
-  service         = "airflow"
-  versioning      = "Enabled"
-}
+#   environment     = var.environment
+#   team            = "forge"
+#   bucket-use-case = "bbss-forecasts"
+#   service         = "airflow"
+#   versioning      = "Enabled"
+# }
 
-module "glaciair_sync_bucket" {
-  source = "../modules/s3-bucket"
+# module "glaciair_sync_bucket" {
+#   source = "../modules/s3-bucket"
 
-  team            = "forge"
-  environment     = var.environment
-  bucket-use-case = "glaci-air-data-lake"
-  service         = "airflow"
-  versioning      = "Enabled"
-}
+#   team            = "forge"
+#   environment     = var.environment
+#   bucket-use-case = "glaci-air-data-lake"
+#   service         = "airflow"
+#   versioning      = "Enabled"
+# }
 
-module "cocosurf_tfstate_bucket" {
-  source = "../modules/s3-bucket"
+# module "cocosurf_tfstate_bucket" {
+#   source = "../modules/s3-bucket"
 
-  environment     = var.environment
-  team            = "forge"
-  bucket-use-case = "cocosurf-gear-tf-state"
-  service         = "Terraform"
-  versioning      = "Enabled"
-}
+#   environment     = var.environment
+#   team            = "forge"
+#   bucket-use-case = "cocosurf-gear-tf-state"
+#   service         = "Terraform"
+#   versioning      = "Enabled"
+# }
 
-module "lonestar_tfstate_bucket" {
-  source = "../modules/s3-bucket"
+# module "lonestar_tfstate_bucket" {
+#   source = "../modules/s3-bucket"
 
-  environment     = var.environment
-  team            = "forge"
-  bucket-use-case = "lonestar-tf-state"
-  service         = "Terraform"
-  versioning      = "Enabled"
-}
+#   environment     = var.environment
+#   team            = "forge"
+#   bucket-use-case = "lonestar-tf-state"
+#   service         = "Terraform"
+#   versioning      = "Enabled"
+# }
 
-module "cgs_tfstate_bucket" {
-  source = "../modules/s3-bucket"
+# module "cgs_tfstate_bucket" {
+#   source = "../modules/s3-bucket"
 
-  environment     = var.environment
-  team            = "forge"
-  bucket-use-case = "cgs-tf-state"
-  service         = "Terraform"
-  versioning      = "Enabled"
-}
+#   environment     = var.environment
+#   team            = "forge"
+#   bucket-use-case = "cgs-tf-state"
+#   service         = "Terraform"
+#   versioning      = "Enabled"
+# }
--- eu-central-1/staging/glue.tf
+++ eu-central-1/staging/glue.tf
@@ -1,9 +1,9 @@
-resource "aws_glue_catalog_database" "federated-engineers-forge-staging-database" {
-  name        = "federated-engineers-forge-staging-database"
-  description = "Catalog database for staging operations"
+# resource "aws_glue_catalog_database" "federated-engineers-forge-staging-database" {
+#   name        = "federated-engineers-forge-staging-database"
+#   description = "Catalog database for staging operations"
 
-  tags = merge(local.common_tags, {
-    region  = var.region
-    service = "Glue"
-  })
-}
+#   tags = merge(local.common_tags, {
+#     region  = var.region
+#     service = "Glue"
+#   })
+# }
