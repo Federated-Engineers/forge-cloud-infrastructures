7389ecad2ac0c9eb9839931444f833a28dfdfb41
feyiarmstrong <solapeajiboye@gmail.com>
2026-07-13 23:56:27 +0100

    deleted staging bucket

--- eu-central-1/staging/data_lake.tf
+++
@@ -1,8 +0,0 @@
-module "forge-data-engineers-staging-bucket" {
-  source          = "../modules/s3_bucket"
-  team            = var.team
-  bucket-use-case = "data-engineers-bucket"
-  service         = "s3"
-  versioning      = "Disabled"
-  environment     = var.environment
-}
