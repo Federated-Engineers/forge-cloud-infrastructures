2ffb837277260445b68e14574d9e0eb334c3be8f
feyiarmstrong <solapeajiboye@gmail.com>
2026-07-13 22:42:12 +0100

    Add BBSS data-lake infrastruture

--- eu-central-1/production/data_lake.tf
+++ eu-central-1/production/data_lake.tf
@@ -24,3 +24,33 @@ resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
     }
   }
 }
+
+
+
+
+module "bbss_bucket" {
+  source          = "../modules/s3_bucket"
+  team            = var.team
+  bucket-use-case = "BBSS-data-lake"
+  service         = "s3"
+  versioning      = "Enabled"
+  environment     = var.environment
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "bbss_bucket_lifecycle" {
+  bucket = module.bbss_bucket.bucket_name
+
+  rule {
+    id     = "transition-to-glacier-instant-retrieval"
+    status = "Enabled"
+
+    filter {
+      prefix = "raw/"
+    }
+
+    transition {
+      days          = 180
+      storage_class = "GLACIER_IR"
+    }
+  }
+}
