c900fb9e716761c4d2bf7c5127fe4e690a7d6866
Anthony Nwachukwu <anthonynwach@gmail.com>
2026-07-16 23:49:56 +0200

    added airflow policy script

---
+++ eu-central-1/production/airflow_policy.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_policy" "airflow_policy" {
+  name        = "${var.team}-airflow-access-policy"
+  description = "Allow Airflow to access aws resources"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Readandwrite"
+        Effect = "Allow"
+        Action = [
+          "s3:*List*",
+          "s3:*Put*",
+          "s3:*Get*",
+          "s3:*object*"
+        ]
+        Resource = [
+          module.scardinavas_bucket.arn,
+          "${module.scardinavas_bucket.arn}/*",
+          module.bbss_bucket.arn,
+          "${module.bbss_bucket.arn}/*",
+          module.mave-aqua-datalake.arn,
+          "${module.mave-aqua-datalake.arn}/*",
+          module.nordic-peaks-oslo.arn,
+          "${module.nordic-peaks-oslo.arn}/*"
+        ]
+      },
+
+      {
+        Sid    = "ReadSSMParameters"
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter",
+          "ssm:GetParameters"
+        ]
+        Resource = [
+          "arn:aws:ssm:eu-central-1:049417293525:parameter/production/google-service-account/credentials",
+          "arn:aws:ssm:eu-central-1:049417293525:parameter/production/forge/bbss/api-key",
+          "arn:aws:ssm:eu-central-1:049417293525:parameter/supabase/database/credentials"
+
+        ]
+      },
+
+      {
+        Sid    = "GlueActions"
+        Effect = "Allow"
+        Action = [
+          "glue:*"
+        ]
+        Resource = ["*"]
+      }
+    ]
+  })
+}
