676d6ad23fc92f655187599245384b406f4b3390
chidubem <dubemnwabuisi@gmail.com>
2026-07-22 23:39:31 +0100

    refactor: remove Nordic Peaks S3 bucket and lifecycle configuration from staging

--- eu-central-1/staging/data_lake.tf
+++ eu-central-1/staging/data_lake.tf
@@ -6,38 +6,3 @@ module "s3_staging_bucket" {
   versioning      = "Enabled"
   environment     = var.environment
 }
-
-
-# Nordic Peaks S3 bucket
-# This bucket is used to store data for the Nordic Peaks project. It has three main zones:
-# - Landing: where raw data is ingested
-# - Processed: where data is processed and transformed
-module "nordic-peaks-oslo" {
-  source          = "../modules/s3_bucket"
-  team            = var.team
-  bucket-use-case = "nordic-peaks-oslo"
-  service         = "s3"
-  versioning      = "Enabled"
-  environment     = var.environment
-}
-
-
-# bucket lifecycle
-resource "aws_s3_bucket_lifecycle_configuration" "nordic_peaks" {
-
-  bucket = module.nordic-peaks-oslo.bucket_name
-
-  rule {
-    id     = "transition-landing-zone-to-Glacier-IR"
-    status = "Enabled"
-
-    filter {
-      prefix = "landing_zone/"
-    }
-
-    transition {
-      days          = 180
-      storage_class = "GLACIER_IR"
-    }
-  }
-}
