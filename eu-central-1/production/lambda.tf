31fdad14e286f387db2157c85705aec603347ce3
feyiarmstrong <solapeajiboye@gmail.com>
2026-08-18 23:12:20 +0100

    add ghost bridge infrastructures

---
+++ eu-central-1/production/lambda.tf
@@ -0,0 +1,31 @@
+resource "aws_lambda_function" "ghost_bridge_lambda" {
+  function_name = "ghost-bridge-lambda"
+  role          = aws_iam_role.ghost_bridge_lambda_role.arn
+
+  runtime = "python3.12"
+  handler = "ghost_bridge.lambda_handler"
+
+  # Lambda deployment package created by CI/CD
+  s3_bucket = "munchen-auto-munich"
+  s3_key    = "lambda/ghost-bridge.zip"
+
+  timeout     = 60
+  memory_size = 512
+
+  architectures = ["x86_64"]
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.ghost_bridge_private_subnet_1.id,
+      aws_subnet.ghost_bridge_private_subnet_2.id
+    ]
+
+    security_group_ids = [
+      aws_security_group.ghost_bridge_lambda_sg.id
+    ]
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "ghost-bridge-lambda"
+  })
+}
