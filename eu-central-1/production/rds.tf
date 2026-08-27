e0ed25149216c66d0de4a22cb7ebfc2685181598
desayoem <labaekaadesayo@gmail.com>
2026-08-27 23:34:42 +0100

    destroy alpine_db_instance

--- eu-central-1/production/rds.tf
+++ eu-central-1/production/rds.tf
@@ -1,23 +1,23 @@
-resource "aws_db_instance" "alpine_db_instance" {
-  allocated_storage           = 100
-  db_name                     = "forge_alpine_heritage"
-  engine                      = "postgres"
-  port                        = 5432
-  engine_version              = "16.11"
-  instance_class              = "db.t3.small"
-  multi_az                    = false
-  db_subnet_group_name        = aws_db_subnet_group.forge_alpine_heritage_db_subnet_group.name
-  vpc_security_group_ids      = [aws_security_group.alpine_db_security_group.id]
-  username                    = "alpine_postgres"
-  manage_master_user_password = true
-  skip_final_snapshot         = true
-  publicly_accessible         = false
-
-
-  tags = {
-    Environment = var.environment
-  }
-}
+# resource "aws_db_instance" "alpine_db_instance" {
+#   allocated_storage           = 100
+#   db_name                     = "forge_alpine_heritage"
+#   engine                      = "postgres"
+#   port                        = 5432
+#   engine_version              = "16.11"
+#   instance_class              = "db.t3.small"
+#   multi_az                    = false
+#   db_subnet_group_name        = aws_db_subnet_group.forge_alpine_heritage_db_subnet_group.name
+#   vpc_security_group_ids      = [aws_security_group.alpine_db_security_group.id]
+#   username                    = "alpine_postgres"
+#   manage_master_user_password = true
+#   skip_final_snapshot         = true
+#   publicly_accessible         = false
+#
+#
+#   tags = {
+#     Environment = var.environment
+#   }
+# }
 
 
 # resource "aws_db_subnet_group" "ghost_bridge_db_subnet_group" {
