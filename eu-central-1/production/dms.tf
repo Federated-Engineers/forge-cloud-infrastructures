fd4953e3d9d21d39400eb2d6cbce1e38a9d57601
Anthony Nwachukwu <anthonynwach@gmail.com>
2026-08-27 23:29:31 +0200

    fixed bug in airflow policy.tf

--- eu-central-1/production/dms.tf
+++ eu-central-1/production/dms.tf
@@ -208,7 +208,7 @@ data "aws_route_tables" "private" {
 }
 
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = "data.aws_vpc.production.id"
+  vpc_id            = data.aws_vpc.production.id
   service_name      = "com.amazonaws.eu-central-1.s3"
   vpc_endpoint_type = "Gateway"
   route_table_ids   = data.aws_route_tables.private.ids
