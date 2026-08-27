3928cf6304fee0ff46711044fdd66cb0550e5894
feyiarmstrong <solapeajiboye@gmail.com>
2026-08-27 09:18:39 +0100

    comment out infra

--- eu-central-1/production/vpc.tf
+++ eu-central-1/production/vpc.tf
@@ -147,113 +147,113 @@
 
 
 
-data "aws_vpc" "ghost_bridge_vpc" {
-  tags = {
-    Name = "secure-production"
-  }
-}
+# data "aws_vpc" "ghost_bridge_vpc" {
+#   tags = {
+#     Name = "secure-production"
+#   }
+# }
 
-data "aws_subnet" "ghost_bridge_private_subnet_1" {
-  vpc_id = data.aws_vpc.ghost_bridge_vpc.id
+# data "aws_subnet" "ghost_bridge_private_subnet_1" {
+#   vpc_id = data.aws_vpc.ghost_bridge_vpc.id
 
-  tags = {
-    Name = "secure-production-private-a"
-  }
-}
+#   tags = {
+#     Name = "secure-production-private-a"
+#   }
+# }
 
-data "aws_subnet" "ghost_bridge_private_subnet_2" {
-  vpc_id = data.aws_vpc.ghost_bridge_vpc.id
+# data "aws_subnet" "ghost_bridge_private_subnet_2" {
+#   vpc_id = data.aws_vpc.ghost_bridge_vpc.id
 
-  tags = {
-    Name = "secure-production-private-b"
-  }
-}
+#   tags = {
+#     Name = "secure-production-private-b"
+#   }
+# }
 
-resource "aws_security_group" "ghost_bridge_lambda_sg" {
-  name        = "ghost-bridge-lambda-sg"
-  description = "Security group for Ghost-Bridge Lambda"
-  vpc_id      = data.aws_vpc.ghost_bridge_vpc.id
+# resource "aws_security_group" "ghost_bridge_lambda_sg" {
+#   name        = "ghost-bridge-lambda-sg"
+#   description = "Security group for Ghost-Bridge Lambda"
+#   vpc_id      = data.aws_vpc.ghost_bridge_vpc.id
 
-  tags = merge(local.common_tags, {
-    Name = "ghost-bridge-lambda-sg"
-  })
-}
+#   tags = merge(local.common_tags, {
+#     Name = "ghost-bridge-lambda-sg"
+#   })
+# }
 
-resource "aws_vpc_security_group_egress_rule" "ghost_bridge_lambda_sg_egress" {
-  security_group_id = aws_security_group.ghost_bridge_lambda_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
+# resource "aws_vpc_security_group_egress_rule" "ghost_bridge_lambda_sg_egress" {
+#   security_group_id = aws_security_group.ghost_bridge_lambda_sg.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1"
+# }
 
-resource "aws_security_group" "ghost_bridge_rds_sg" {
-  name        = "ghost-bridge-rds-sg"
-  description = "Security group for Ghost-Bridge PostgreSQL RDS"
-  vpc_id      = data.aws_vpc.ghost_bridge_vpc.id
+# resource "aws_security_group" "ghost_bridge_rds_sg" {
+#   name        = "ghost-bridge-rds-sg"
+#   description = "Security group for Ghost-Bridge PostgreSQL RDS"
+#   vpc_id      = data.aws_vpc.ghost_bridge_vpc.id
 
-  tags = merge(local.common_tags, {
-    Name = "ghost-bridge-rds-sg"
-  })
-}
+#   tags = merge(local.common_tags, {
+#     Name = "ghost-bridge-rds-sg"
+#   })
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "ghost_bridge_rds_from_lambda" {
-  security_group_id            = aws_security_group.ghost_bridge_rds_sg.id
-  referenced_security_group_id = aws_security_group.ghost_bridge_lambda_sg.id
+# resource "aws_vpc_security_group_ingress_rule" "ghost_bridge_rds_from_lambda" {
+#   security_group_id            = aws_security_group.ghost_bridge_rds_sg.id
+#   referenced_security_group_id = aws_security_group.ghost_bridge_lambda_sg.id
 
-  from_port   = 5432
-  to_port     = 5432
-  ip_protocol = "tcp"
-}
+#   from_port   = 5432
+#   to_port     = 5432
+#   ip_protocol = "tcp"
+# }
 
-resource "aws_vpc_security_group_egress_rule" "ghost_bridge_rds_sg_egress" {
-  security_group_id = aws_security_group.ghost_bridge_rds_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
+# resource "aws_vpc_security_group_egress_rule" "ghost_bridge_rds_sg_egress" {
+#   security_group_id = aws_security_group.ghost_bridge_rds_sg.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1"
+# }
 
-resource "aws_security_group" "ghost_bridge_secrets_endpoint_sg" {
-  name        = "ghost-bridge-secrets-endpoint-sg"
-  description = "Security group for Ghost-Bridge Secrets Manager VPC endpoint"
-  vpc_id      = data.aws_vpc.ghost_bridge_vpc.id
+# resource "aws_security_group" "ghost_bridge_secrets_endpoint_sg" {
+#   name        = "ghost-bridge-secrets-endpoint-sg"
+#   description = "Security group for Ghost-Bridge Secrets Manager VPC endpoint"
+#   vpc_id      = data.aws_vpc.ghost_bridge_vpc.id
 
-  tags = merge(local.common_tags, {
-    Name = "ghost-bridge-secrets-endpoint-sg"
-  })
-}
+#   tags = merge(local.common_tags, {
+#     Name = "ghost-bridge-secrets-endpoint-sg"
+#   })
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "ghost_bridge_secrets_from_lambda" {
-  security_group_id            = aws_security_group.ghost_bridge_secrets_endpoint_sg.id
-  referenced_security_group_id = aws_security_group.ghost_bridge_lambda_sg.id
+# resource "aws_vpc_security_group_ingress_rule" "ghost_bridge_secrets_from_lambda" {
+#   security_group_id            = aws_security_group.ghost_bridge_secrets_endpoint_sg.id
+#   referenced_security_group_id = aws_security_group.ghost_bridge_lambda_sg.id
 
-  from_port   = 443
-  to_port     = 443
-  ip_protocol = "tcp"
-}
+#   from_port   = 443
+#   to_port     = 443
+#   ip_protocol = "tcp"
+# }
 
-resource "aws_vpc_security_group_egress_rule" "ghost_bridge_secrets_endpoint_egress" {
-  security_group_id = aws_security_group.ghost_bridge_secrets_endpoint_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
+# resource "aws_vpc_security_group_egress_rule" "ghost_bridge_secrets_endpoint_egress" {
+#   security_group_id = aws_security_group.ghost_bridge_secrets_endpoint_sg.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1"
+# }
 
-resource "aws_vpc_endpoint" "ghost_bridge_secrets_manager" {
-  vpc_id = data.aws_vpc.ghost_bridge_vpc.id
+# resource "aws_vpc_endpoint" "ghost_bridge_secrets_manager" {
+#   vpc_id = data.aws_vpc.ghost_bridge_vpc.id
 
-  service_name = "com.amazonaws.${var.region}.secretsmanager"
+#   service_name = "com.amazonaws.${var.region}.secretsmanager"
 
-  vpc_endpoint_type = "Interface"
+#   vpc_endpoint_type = "Interface"
 
-  subnet_ids = [
-    data.aws_subnet.ghost_bridge_private_subnet_1.id,
-    data.aws_subnet.ghost_bridge_private_subnet_2.id
-  ]
+#   subnet_ids = [
+#     data.aws_subnet.ghost_bridge_private_subnet_1.id,
+#     data.aws_subnet.ghost_bridge_private_subnet_2.id
+#   ]
 
-  security_group_ids = [
-    aws_security_group.ghost_bridge_secrets_endpoint_sg.id
-  ]
+#   security_group_ids = [
+#     aws_security_group.ghost_bridge_secrets_endpoint_sg.id
+#   ]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = merge(local.common_tags, {
-    Name = "ghost-bridge-secrets-manager-endpoint"
-  })
-}
+#   tags = merge(local.common_tags, {
+#     Name = "ghost-bridge-secrets-manager-endpoint"
+#   })
+# }
