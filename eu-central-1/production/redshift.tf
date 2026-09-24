82fbba003c95c5993a7f258d3b84dce0f57a886e
desayoem <labaekaadesayo@gmail.com>
2026-09-24 20:22:30 +0100

    Add SpreeKauf analytics Redshift cluster, subnet group, and security group
    - Add aws_redshift_cluster.spreekauf_analytics_db_cluster
    - Add aws_security_group.spreekauf_db_security_group with ingress on 5439
      scoped to prod_vpc CIDR, and open egress

---
+++ eu-central-1/production/redshift.tf
@@ -0,0 +1,11 @@
+resource "aws_redshift_cluster" "spreekauf_analytics_db_cluster" {
+  cluster_identifier        = "spreekauf-redshift-cluster"
+  database_name             = "spreekaufdb"
+  master_username           = "spreekauf_redshift"
+  node_type                 = "ra3.large"
+  cluster_type              = "single-node"
+  cluster_subnet_group_name = forge_spreekauf_analytics_redshift_subnet_group
+  vpc_security_group_ids    = [aws_security_group.spreekauf_redshift_sg.id]
+  publicly_accessible       = false
+  manage_master_password    = true
+}
--- eu-central-1/production/security_groups.tf
+++ eu-central-1/production/security_groups.tf
@@ -21,3 +21,29 @@ resource "aws_vpc_security_group_ingress_rule" "postgres_rds_ingress" {
   ip_protocol = "tcp"
   to_port     = 5432
 }
+
+
+resource "aws_security_group" "spreekauf_db_security_group" {
+  name        = "spreekauf-db-security-group"
+  description = "Enable ingress on port 5439 and all outbound traffic from Spreekauf DB"
+  vpc_id      = data.aws_vpc.prod_vpc.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "spreekauf_redshift_ingress" {
+  security_group_id = aws_security_group.spreekauf_db_security_group.id
+  cidr_ipv4         = data.aws_vpc.prod_vpc.cidr_block
+
+  from_port   = 5439
+  ip_protocol = "tcp"
+  to_port     = 5439
+}
+
+resource "aws_vpc_security_group_egress_rule" "spreekauf_redshift_egress" {
+  security_group_id = aws_security_group.spreekauf_db_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+
+  from_port   = 0
+  ip_protocol = "-1"
+  to_port     = 0
+}
+
