1ee83671f1ef44ce4e3cc4991636e20263339334
desayoem <labaekaadesayo@gmail.com>
2026-09-24 20:50:39 +0100

    format wlm JSON configuration

--- eu-central-1/production/redshift.tf
+++ eu-central-1/production/redshift.tf
@@ -1,13 +1,13 @@
 resource "aws_redshift_cluster" "spreekauf_analytics_db_cluster" {
-  cluster_identifier        = "spreekauf-redshift-cluster"
-  database_name             = "spreekaufdb"
-  master_username           = "spreekauf_redshift"
-  node_type                 = "ra3.large"
-  cluster_type              = "single-node"
-  cluster_subnet_group_name = "forge_spreekauf_analytics_redshift_subnet_group"
-  vpc_security_group_ids    = [aws_security_group.spreekauf_db_security_group.id]
-  publicly_accessible       = false
-  manage_master_password    = true
+  cluster_identifier           = "spreekauf-redshift-cluster"
+  database_name                = "spreekaufdb"
+  master_username              = "spreekauf_redshift"
+  node_type                    = "ra3.large"
+  cluster_type                 = "single-node"
+  cluster_subnet_group_name    = "forge_spreekauf_analytics_redshift_subnet_group"
+  vpc_security_group_ids       = [aws_security_group.spreekauf_db_security_group.id]
+  publicly_accessible          = false
+  manage_master_password       = true
   cluster_parameter_group_name = aws_redshift_parameter_group.spreekauf_db_wlm.name
 }
 
@@ -17,7 +17,7 @@ resource "aws_redshift_parameter_group" "spreekauf_db_wlm" {
   family = "redshift-1.0"
 
   parameter {
-    name  = "wlm_json_configuration"
+    name = "wlm_json_configuration"
     value = jsonencode([
       {
 
