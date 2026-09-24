b0a2fc0207fcd21e7e6c0e42e16b3e23785b61fb
desayoem <labaekaadesayo@gmail.com>
2026-09-24 20:25:24 +0100

    modify security group name

--- eu-central-1/production/redshift.tf
+++ eu-central-1/production/redshift.tf
@@ -5,7 +5,7 @@ resource "aws_redshift_cluster" "spreekauf_analytics_db_cluster" {
   node_type                 = "ra3.large"
   cluster_type              = "single-node"
   cluster_subnet_group_name = forge_spreekauf_analytics_redshift_subnet_group
-  vpc_security_group_ids    = [aws_security_group.spreekauf_redshift_sg.id]
+  vpc_security_group_ids    = [aws_security_group.spreekauf_db_security_group.id]
   publicly_accessible       = false
   manage_master_password    = true
 }
